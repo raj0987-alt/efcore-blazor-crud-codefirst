63b748be9dc12a4dd57246071a512f201ae99f2a
raj0987-alt <71288207+raj0987-alt@users.noreply.github.com>
2023-06-21 09:56:44 +0600

    Static Get Commit

---
+++ Server/Controllers/EmployeesController.cs
@@ -0,0 +1,55 @@
+﻿using AspDotNetCoreBlazorFullStackCrud.Shared.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspDotNetCoreBlazorFullStackCrud.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeesController : ControllerBase
+    {
+        public static List<Skill> skills = new List<Skill> {
+            new Skill {Id =1, Name = "ASP.NET"},
+            new Skill {Id = 2, Name ="React JS"}
+        };
+
+        public static List<Employee> employees = new List<Employee>
+        {
+            new Employee
+            {
+                Id = 1,
+                Name = "Jon Doe",
+                FirstName = "Jon",
+                LastName = "Doe",
+                Email = "jon@email.com",
+                Skill = skills [0]
+            },
+            new Employee
+            {
+                Id = 2,
+                Name = "Mike Hussi",
+                FirstName = "Mike",
+                LastName = "Hussi",
+                Email = "mike@email.com",
+                Skill = skills [1]
+            }
+        };
+        [HttpGet]
+        public async Task<ActionResult<List<Employee>>> GetEmployee()
+        {
+            
+            return Ok(employees);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<List<Employee>>> GetSingleEmployee(int id)
+        {
+            var employee = employees.FirstOrDefault(e=>e.Id == id);
+            if (employee == null)
+            {
+                return NotFound("Sorry, no employee found. :/");
+            }
+            return Ok(employee);
+        }
+    }
+}
---
+++ Shared/Models/Employee.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AspDotNetCoreBlazorFullStackCrud.Shared.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public Skill? Skill { get; set; }
+        public int SkillId { get; set; }
+    }
+}
